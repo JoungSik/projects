c3a7503e28c334d2321ed877dde4610f85565a7a
Joungsik <tjstlr2010@gmail.com>
2025-03-14 22:01:37 +0900

    [Done] solid_cable 미사용으로 삭제

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-2.times do |i|
-  user = User.create(name: "test #{i}", email: "test#{i}@test.com",
-                     password: 'qwer1234', password_confirmation: 'qwer1234')
-
-  2.times do |j|
-    workspace = Workspace.create(name: "Workspace #{i}#{j}",
-                                 workspace_users_attributes: { '0': { user: user, role: WorkspaceUser.roles[:owner] } })
-    5.times do |k|
-      project = Project.create(title: "Project #{j}#{k}", description: "Description #{j}#{k}",
-                               workspace: workspace, owner: user, creator: user)
-
-      10.times do |l|
-        r = Random.new
-        Task.create(title: "Task #{j}#{k}#{l}", description: "Description #{j}#{k}#{l}",
-                    project: project, assign_user: user, creator: user,
-                    start_at: Date.today - r.rand(0...4), end_at: Date.today + r.rand(0...4))
-      end
-    end
-  end
-end
