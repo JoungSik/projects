9fae3d0a26a68e76f8858a45f6e754d2f724fa8e
Joungsik <tjstlr2010@gmail.com>
2025-03-21 01:58:18 +0900

    [Fix] Task 진행중 상태 색상 파란색으로 변경

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -25,11 +25,13 @@ module TasksHelper
   end
 
   # class="border-gray-200 bg-gray-100 text-gray-700"
+  # class="border-blue-200 bg-blue-100 text-blue-700"
   # class="border-red-200 bg-red-100 text-red-700"
   # class="border-green-200 bg-green-100 text-green-700"
   def task_status_color(task)
     case task.status_before_type_cast
-    when 0, 1 then "gray"
+    when 0 then "gray"
+    when 1 then "blue"
     when 2, 4 then "red"
     when 3 then "green"
     else "gray"
