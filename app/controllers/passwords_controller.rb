e66cecc6477f9b43bc9c95e0ce38e0c877dddab0
JoungSik <tjstlr2010@gmail.com>
2025-03-20 20:39:14 +0900

    [Done] 비밀번호 변경 이메일 요청 페이지 구현완료

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -10,7 +10,7 @@ class PasswordsController < ApplicationController
       PasswordsMailer.reset(user).deliver_later
     end
 
-    redirect_to login_path, notice: "Password reset instructions sent (if user with that email address exists)."
+    redirect_to login_path, notice: t("passwords.messages.create")
   end
 
   def edit
@@ -18,9 +18,9 @@ class PasswordsController < ApplicationController
 
   def update
     if @user.update(params.permit(:password, :password_confirmation))
-      redirect_to login_path, notice: "Password has been reset."
+      redirect_to login_path, notice: t("passwords.messages.update")
     else
-      redirect_to edit_password_path(params[:token]), alert: "Passwords did not match."
+      redirect_to edit_password_path(params[:token]), alert: t("passwords.messages.error_update")
     end
   end
 
@@ -29,6 +29,6 @@ class PasswordsController < ApplicationController
   def set_user_by_token
     @user = User.find_by_password_reset_token!(params[:token])
   rescue ActiveSupport::MessageVerifier::InvalidSignature
-    redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
+    redirect_to new_password_path, alert: t("passwords.messages.invalid_token")
   end
 end
