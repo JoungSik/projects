adba0262898c9e550b40e6afaa89439e03ab8443
Joungsik <tjstlr2010@gmail.com>
2025-03-29 01:59:53 +0900

    [Fix] due_status 조건에 cancelled 오타 수정

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     @tasks_by_status_count[:total] = @tasks_by_status_count.values.sum
 
     @tasks_by_due_status = Task.includes(:project).where(assign_user: @current_user, end_at: ..Date.today.end_of_week)
-                               .where.not(status: [ :completed, :canceled ])
+                               .where.not(status: [ :completed, :cancelled ])
                                .each_with_object({ due_today: [], due_this_week: [], overdue: [] }) do |task, hash|
       case task.end_at
       when ...Date.today.beginning_of_week then hash[:overdue] << task
