d72a358c3a03122364a8ee17b18493641e045656
Joungsik <tjstlr2010@gmail.com>
2025-03-30 01:42:13 +0900

    [Fix] Sidebar 항상 active 상태인것처럼 보이던 이슈 수정

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,14 @@ module ApplicationHelper
     enum_hash = model_class.send(enum.to_s.pluralize)
 
     enum_hash.map do |key, _|
-      [t("activerecord.attributes.#{model_class.to_s.downcase}.#{enum}.#{key}"), key]
+      [ t("activerecord.attributes.#{model_class.to_s.downcase}.#{enum}.#{key}"), key ]
     end
+  end
+
+  # class="text-gray-700 text-indigo-700 bg-indigo-50"
+  def sidebar_color(workspace_id, current_workspace_id = nil)
+    return "text-indigo-700 bg-indigo-50" if workspace_id == current_workspace_id
+
+    "text-gray-700"
   end
 end
