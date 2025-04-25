4a9d6d38336ccdc3d55e6ef105d301af41907d17
Joungsik <tjstlr2010@gmail.com>
2025-04-25 16:44:22 +0900

    [Done] rubocop 도입 및 수정

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,5 +1,4 @@
 class InvitationMailer < ApplicationMailer
-
   def invite(user_invitation)
     @user_invitation = user_invitation
     mail subject: t("email.invitation.invite.subject", app_name: t("app.name")), to: user_invitation.invitee_email
